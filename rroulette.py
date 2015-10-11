e57b2b5eb1baa5b34e7cc8a38bb9b7f2d21065fa
WZ1 <legendary.hero.wz1@gmail.com>
2015-10-10 22:49:26 -0400

    Update and rename roulette.py to rroulette.py
    Added a chamber-count changing command
    Timeouts are for nerds, so i got rid of that too.

--- rroulette.py
+++ rroulette.py
@@ -6,28 +6,28 @@ Copyright 2015 dgw
 from __future__ import division
 from willie import module
 import random
-import time
 
-
-TIMEOUT = 600
-
+CHAMBERS=6
 
 @module.commands('roulette')
 @module.require_chanmsg
 def roulette(bot, trigger):
-    time_since = time_since_roulette(bot, trigger.nick)
-    if time_since < TIMEOUT:
-        bot.notice("You must wait %d seconds until your next roulette attempt." % (TIMEOUT - time_since), trigger.nick)
-        return module.NOLIMIT
-    if 6 != random.randint(1, 6):
+    if CHAMBERS != random.randint(1, CHAMBERS):
         won = True
         bot.say("Click! %s is lucky; there was no bullet." % trigger.nick)
     else:
         won = False
         bot.say("BANG! %s is dead!" % trigger.nick)
-    bot.db.set_nick_value(trigger.nick, 'roulette_last', time.time())
     update_roulettes(bot, trigger.nick, won)
 
+@module.commands('chambers')
+@module.require_chanmsg
+def chambers(bot,trigger):
+	global CHAMBERS
+        if not trigger.group(3):
+            bot.reply("No chamber count specified.")
+	CHAMBERS = int(trigger.group(3))
+	bot.reply("'Tis done, My Master!")
 
 @module.commands('roulettes', 'r')
 def roulettes(bot, trigger):
@@ -36,9 +36,8 @@ def roulettes(bot, trigger):
     if not games:
         bot.say("%s hasn't played Russian roulette yet." % target)
         return
-    bot.say("%s has survived Russian roulette %d out of %d times (or %.2f%%)."
+    bot.say("%s has survived Russian roulette %d out of %d times. Survival rate: %.2f%%"
             % (target, wins, games, wins / games * 100))
-
 
 def update_roulettes(bot, nick, won=False):
     games, wins = get_roulettes(bot, nick)
@@ -53,9 +52,3 @@ def get_roulettes(bot, nick):
     games = bot.db.get_nick_value(nick, 'roulette_games') or 0
     wins = bot.db.get_nick_value(nick, 'roulette_wins') or 0
     return games, wins
-
-
-def time_since_roulette(bot, nick):
-    now = time.time()
-    last = bot.db.get_nick_value(nick, 'roulette_last') or 0
-    return abs(now - last)
