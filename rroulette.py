f5c684c0227f3bc277cd29d5b520d30cec79ba82
WZ1 <legendary.hero.wz1@gmail.com>
2015-10-16 22:48:51 -0400

    Chambercheck loop and reset
    Made my chamber-check loop a function and added admin-only reset. Resets are always fun.

--- rroulette.py
+++ rroulette.py
@@ -4,56 +4,51 @@ Copyright 2015 dgw, WZ1
 """
 from __future__ import division
 import random
-from sopel import module
+from sopel import module,tools
 
 CHAMBERS=6
-blanks = [0]
+blanks = []
 chamber=0
+
 @module.commands('rroulette','russia','rr')
 @module.require_chanmsg
 def rroulette(bot, trigger):
-   global blanks,chamber
-   chamber = random.randint(1, CHAMBERS)
-   while CHAMBERS != chamber:
-      if chamber not in blanks:
-         blanks.extend([chamber])
-         won = True
-         bot.say("Click! %s is lucky; there was no bullet." % trigger.nick)
-         break
-      else:
-         chamber = random.randint(1, CHAMBERS)
-   else:
-      blanks = [0]
-      won = False
+   global blanks
+   if chambercheck()==True:
+      won=False
       bot.say("BANG! %s is dead!" % trigger.nick)
-   update_roulettes(bot, trigger.nick, won)
+      update_roulettes(bot, trigger.nick, won)
+   if chambercheck()==False:
+      won=True
+      bot.say("Click! %s is lucky; there was no bullet." % trigger.nick)
+      update_roulettes(bot, trigger.nick, won)
     
 @module.commands('shoot')
 @module.require_chanmsg
 def shoot(bot,trigger):
-   global blanks,chamber
-   chamber = random.randint(1, CHAMBERS)
-   target = trigger.group(3)
+   global chamber
+   target = tools.Identifier(trigger.group(3) or '')
    if not target:
       bot.reply("Who did you want to shoot?")
       return module.NOLIMIT
    if target.lower() not in bot.privileges[trigger.sender.lower()]:
-      bot.say("You can't shoot people who don't exist!")
+      bot.say("Error 404. Nick not found.")
       return module.NOLIMIT 
    if target == bot.nick:
       bot.say("You can't shoot me! How rude!")
-      return module.NOLIMIT 
-   if chamber == random.randint(1,CHAMBERS):
-      bot.say("%s shoots %s" % (trigger.nick, target))
-      won=True
+      return module.NOLIMIT
+   if chambercheck()==True:
       targetwin=False
-   else:
-      bot.say("The chamber was blank. %s loses. Chambers reloaded." % trigger.nick)
-      won=False
+      won=True
+      bot.say("%s has shot %s dead! Chambers reset." % (trigger.nick,target))
+      update_roulettes(bot, trigger.nick, won)
+      update_roulettes(bot, target, targetwin)
+   if chambercheck()==False:
       targetwin=True
-   blanks = [0]
-   update_roulettes(bot, trigger.nick, won)
-   update_roulettes(bot, target, targetwin)
+      won=False
+      bot.say("The chamber was blank. %s fails to kill %s. Chambers reloaded." % (trigger.nick,target))
+      update_roulettes(bot, trigger.nick, won)
+      update_roulettes(bot, target, targetwin)
 
 @module.commands('chambers')
 @module.require_admin
@@ -63,7 +58,8 @@ def chambers(bot,trigger):
    if not trigger.group(3):
       bot.reply("No chamber count specified.")
    CHAMBERS = int(trigger.group(3))
-   bot.reply("'Tis done, My Master!")
+   blanks = []
+   bot.say("'Tis done, My Master!")
 
 @module.commands('rroulettes', 'rrs')
 def rroulettes(bot, trigger):
@@ -75,6 +71,23 @@ def rroulettes(bot, trigger):
     bot.say("%s has survived chamber-based Russian roulette %d out of %d times. Survival rate: %.2f%%"
             % (target, wins, games, wins / games * 100))
 
+@module.commands('resetrr','russianreset','rrr')
+@module.require_admin
+def resetrr(bot, trigger):
+   target = tools.Identifier(trigger.group(3) or '')
+   if not target :
+      bot.db.set_nick_value(trigger.nick, 'rroulette_games', 0)
+      bot.db.set_nick_value(trigger.nick, 'rroulette_wins', 0)
+      bot.say("'Tis done, My Master!")
+      return module.NOLIMIT
+   elif target == bot.nick:
+      bot.say("Ha! Joke's on you! I don't even have stats to reset! You still can't touch them though...")
+      return module.NOLIMIT
+   if target :
+      bot.db.set_nick_value(target, 'rroulette_games', 0)
+      bot.db.set_nick_value(target, 'rroulette_wins', 0)
+      bot.say("'Tis done, My Master!")
+   
 def update_roulettes(bot, nick, won=False):
     games, wins = get_roulettes(bot, nick)
     games += 1
@@ -83,8 +96,23 @@ def update_roulettes(bot, nick, won=False):
     bot.db.set_nick_value(nick, 'rroulette_games', games)
     bot.db.set_nick_value(nick, 'rroulette_wins', wins)
 
-
 def get_roulettes(bot, nick):
     games = bot.db.get_nick_value(nick, 'rroulette_games') or 0
     wins = bot.db.get_nick_value(nick, 'rroulette_wins') or 0
     return games, wins
+
+def chambercheck():
+   global blanks,CHAMBERS,chamber
+   chamber = random.randint(1, CHAMBERS)
+   while CHAMBERS != chamber:
+      if chamber not in blanks:
+         blanks.extend([chamber])
+         dead = False
+         break
+      else:
+         chamber = random.randint(1, CHAMBERS)
+   if CHAMBERS == chamber:
+      blanks = []
+      dead = True
+   return dead
+   
